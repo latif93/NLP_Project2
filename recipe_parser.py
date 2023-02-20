534299f22639c4e89df77a3ee799c0a28fb6642c
jadalking <113944159+jadalking@users.noreply.github.com>
2023-02-20 16:51:41 -0600

    working with sentences

--- recipe_parser.py
+++ recipe_parser.py
@@ -1,6 +1,7 @@
-from bs4 import BeautifulSoup #webscraper
-import requests #html request maker
+from bs4 import BeautifulSoup  # webscraper
+import requests  # html request maker
 import spacy
+
 
 nlp = spacy.load("en_core_web_sm")
 url = "https://www.bhg.com/recipes/how-to/bake/how-to-make-cupcakes/"
@@ -9,5 +10,10 @@ soup = BeautifulSoup(r.content, 'html.parser')
 print(soup.text)
 
 recipe_doc = nlp(soup.text)
-for token in recipe_doc:
-	print(token, token.idx)
+# for token in recipe_doc:
+# 	print(token, token.idx)
+
+sentences = list(recipe_doc.sents)
+
+for sentence in sentences:
+	print(f'{sentence[:5]}...')
