2993d81874518c3cdfd335cf787d54ae9531a471
latif93 <108307359+latif93@users.noreply.github.com>
2023-02-20 16:25:48 -0600

    recipe parser experimenting

---
+++ recipe_parser.py
@@ -0,0 +1,13 @@
+from bs4 import BeautifulSoup #webscraper
+import requests #html request maker
+import spacy
+
+nlp = spacy.load("en_core_web_sm")
+url = "https://www.bhg.com/recipes/how-to/bake/how-to-make-cupcakes/"
+r = requests.get(url)
+soup = BeautifulSoup(r.content, 'html.parser')
+print(soup.text)
+
+recipe_doc = nlp(soup.text)
+for token in recipe_doc:
+	print(token, token.idx)
