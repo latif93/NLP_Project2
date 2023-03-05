e73aa94faa43720dddafa9cae4b292a80ac63827
jadalking <113944159+jadalking@users.noreply.github.com>
2023-03-04 19:14:28 -0600

    extracting ingredients for each step

--- recipe_parser.py
+++ recipe_parser.py
@@ -34,14 +34,17 @@ class Step:
 
     def __str__(self):
         output = f'STEP text:\n   {self.text}\n' \
-                 f'ACTIONS: {self.actions}\n\n'
+                 f'ACTIONS: {self.actions}\n' \
+                 f'INGREDIENTS: {self.ingredients}\n' \
+                 f'MISCELLANEOUS: {self.misc}\n\n'
         return output
 
 
 # TODO by "You will need a lexicon of words used in recipes corresponding to this simple semantic model.", I think
 #  they mean that we should have a hard-coded structure of words that would most commonly show up???
-
 # just used chatgpt to generate some words for now, but basically we could use the lexicon for the type checking system
+#
+# TODO all generated, might need to think of more words ourselves to account for
 lexicon = {
     "actions": ["add", "adding", "bake", "baking", "beat", "beating", "blend", "blending", "boil", "boiling", "broil",
                 "broiling", "brush", "brushing", "chill", "chilling", "coat", "coating", "combine", "combining", "cook",
@@ -54,38 +57,32 @@ lexicon = {
                 "roast", "roasting", "saute", "sauteing", "season", "seasoning", "shred", "shredding", "simmer",
                 "simmering", "squeeze", "squeezing", "stir", "stirring", "strain", "straining", "stuff", "stuffing",
                 "thicken", "thickening", "toast", "toasting", "toss", "tossing", "whip", "whipping", "whisk",
-                "whisking",  # <-- TODO all generated, might need to think of more
-                "prepare", "preparing", "allow", "allowing", "set", "setting"],  # other words I thought of
-    "ingredients": [],
-    "utensils": [],
-    "parameters": []}   # TODO look for numbers that represent things like time and temperature
+                "whisking", "prepare", "preparing", "allow", "allowing", "set", "setting"],
+    "ingredients": ['salt', 'sugar', 'flour', 'egg', 'eggs', 'butter', 'oil', 'milk', 'cheese', 'garlic', 'onion',
+                    'tomato', 'pepper', 'chicken', 'beef', 'pork', 'fish', 'shrimp', 'rice', 'pasta', 'bean', 'beans',
+                    'potato', 'potatoes', 'carrot', 'carrots', 'celery', 'bell pepper', 'bell peppers', 'broccoli',
+                    'cauliflower', 'spinach', 'lettuce', 'cucumber', 'avocado', 'lemon', 'lime', 'orange', 'apple',
+                    'banana', 'strawberry', 'strawberries', 'blueberry', 'blueberries', 'raspberry', 'raspberries',
+                    'almond', 'almonds', 'walnut', 'walnuts', 'cashew', 'cashews', 'sunflower seed', 'sunflower seeds',
+                    'pumpkin seed', 'pumpkin seeds', 'sesame seed', 'sesame seeds', 'basil', 'parsley', 'cilantro',
+                    'cumin', 'paprika', 'cinnamon', 'vinegar', 'soy sauce', 'mustard', 'ketchup', 'mayonnaise', 'honey',
+                    'maple syrup', 'chocolate', 'vanilla extract', 'baking powder', 'baking soda', 'yeast'],
+    "utensils": ['bowl', 'mixing bowl', 'whisk', 'spoon', 'fork', 'knife', 'cutting board', 'grater', 'peeler',
+                 'colander', 'strainer', 'measuring cup', 'measuring spoon', 'rolling pin', 'pastry brush', 'oven mitt',
+                 'pot', 'pan', 'skillet', 'saucepan', 'ladle', 'tongs', 'spatula', 'slotted spoon', 'baking dish',
+                 'baking sheet', 'pie dish', 'casserole dish', 'grill pan', 'wok', 'slow cooker', 'blender',
+                 'food processor', 'hand mixer', 'stand mixer', 'pastry cutter'],
+    "parameters": []}  # TODO look for numbers that represent things like time and temperature
 
 nlp = spacy.load("en_core_web_sm")
+
+# TODO Make it work w any url and test accordingly
 url = "https://www.bhg.com/recipes/how-to/bake/how-to-make-cupcakes/"
 r = requests.get(url)
 soup = BeautifulSoup(r.content, 'html.parser')
 # print(soup.text)
 
 recipe_doc = nlp(soup.text)  # extract text from site
-token_by_part_of_speech = {}
-for token in recipe_doc:  # filling dict of tokens by the part of speech assigned to them by en_core_web_sm dependency parser
-    if token.pos_ not in token_by_part_of_speech:
-        token_by_part_of_speech[token.pos_] = []
-    else:
-        token_by_part_of_speech[token.pos_].append(token)
-
-token_by_part_of_speech.pop('SPACE')  # removing space from the dictionary to focus on words
-token_by_part_of_speech.pop('PUNCT')  # removing punctuation from the dictionary to focus on words
-token_by_part_of_speech.pop('SYM')  # removing symbols from the dictionary to focus on words
-for key in token_by_part_of_speech.keys():
-    token_by_part_of_speech[key] = list(set(token_by_part_of_speech[key]))
-    token_by_part_of_speech[key] = [str(x).lower() for x in token_by_part_of_speech[key] if
-                                    "." not in str(x) and "#" not in str(x) and ";" not in str(x) and "}" not in str(
-                                        x) and "{" not in str(x) and "@" not in str(x)]
-# removed duplicates from lists and filtered out some trash from the lists (also lowercased all tokens)
-
-# print(token_by_part_of_speech)
-# print(token_by_part_of_speech.keys())
 
 sentences = list(recipe_doc.sents)
 
@@ -95,19 +92,61 @@ or maybe try to figure out how to typecheck nouns for ingredients or something l
 stop here for now. doing any of this other stuff seems pretty simple anyway.
 """
 
-# TODO I think we could use the dependency parsing for each sentence to improve this idea:
-# General idea for now. it does get decent enough results in an okay amount of time.
-for sentence in sentences:
-    sentence = str(sentence).replace('\n', '').lower()
-    sentence_verbs = []
 
-    for verb in lexicon["actions"]:  # typechecking to see if the sentence contains any cooking actions
-        if f' {verb} ' in sentence:  # ignore words in which the verb is a subset of it (e.g. "BAKEry" or "COOKie")
-            sentence_verbs.append(verb)
+# For a given step, find out ingredients and tools used for it
+def extract_items(parts_of_speech):
+    ingredients = []
+    utensils = []
+    if 'NOUN' in parts_of_speech and parts_of_speech['NOUN']:
+        for noun in parts_of_speech['NOUN']:
+            noun = str(noun)
+            if noun in lexicon['ingredients']:
+                ingredients.append(noun)
+            elif noun in lexicon['utensils']:
+                utensils.append(noun)
+    return ingredients, utensils
 
-    if len(sentence_verbs) > 0:
-        new_step = Step(sentence, sentence_verbs)
-        all_steps.append(new_step)
+
+# Do dependency parsing on each sentence. If any of the verbs are cooking verbs, then it is a valid step
+# and we store the step and dependency parsing results. Basically initializing every step
+def parse_sentences():
+    for sentence in sentences:
+        sentence = str(sentence).replace('\n', '').lower()
+        sent_obj = nlp(sentence)
+        sentence_verbs = []
+
+        # TODO: dependency parsing misses phrases like "hand mixer" and doesn't count them as nouns although they're
+        #  valid tools to use
+        token_by_part_of_speech = {}
+        for token in sent_obj:  # filling dict of tokens by the part of speech assigned to them by en_core_web_sm dependency parser
+            if token.pos_ not in token_by_part_of_speech:
+                token_by_part_of_speech[token.pos_] = []
+            else:
+                token_by_part_of_speech[token.pos_].append(token)
+
+        # check if the sentence has verbs in it, specificaly cooking verbs
+        if 'VERB' in token_by_part_of_speech and token_by_part_of_speech['VERB']:
+            for verb in token_by_part_of_speech['VERB']:
+                if str(verb) in lexicon["actions"]:
+                    sentence_verbs.append(verb)
+
+            # if cooking verbs were found, filter out the parts of speech dict, and
+            # create a new Step object and store it
+            if len(sentence_verbs) > 0:
+                for key in token_by_part_of_speech.keys():
+                    token_by_part_of_speech[key] = list(set(token_by_part_of_speech[key]))
+                    token_by_part_of_speech[key] = [str(x).lower() for x in token_by_part_of_speech[key] if
+                                                    "." not in str(x) and "#" not in str(x) and ";" not in str(
+                                                        x) and "}" not in str(
+                                                        x) and "{" not in str(x) and "@" not in str(x)]
+                new_step = Step(sentence, sentence_verbs)
+
+                # extract the ingredients and utensils
+                new_step.ingredients, new_step.misc = extract_items(token_by_part_of_speech)
+                all_steps.append(new_step)
+
+
+parse_sentences()
 
 for step in all_steps:
     print(step)
@@ -116,3 +155,25 @@ for step in all_steps:
 #  for every step we have found, then figure out the ingredients, utensils, parameters, etc.
 
 
+
+
+''' Original dependency parsing code below: '''
+# token_by_part_of_speech = {}
+# for token in recipe_doc:  # filling dict of tokens by the part of speech assigned to them by en_core_web_sm dependency parser
+#     if token.pos_ not in token_by_part_of_speech:
+#         token_by_part_of_speech[token.pos_] = []
+#     else:
+#         token_by_part_of_speech[token.pos_].append(token)
+#
+# token_by_part_of_speech.pop('SPACE')  # removing space from the dictionary to focus on words
+# token_by_part_of_speech.pop('PUNCT')  # removing punctuation from the dictionary to focus on words
+# token_by_part_of_speech.pop('SYM')  # removing symbols from the dictionary to focus on words
+# for key in token_by_part_of_speech.keys():
+#     token_by_part_of_speech[key] = list(set(token_by_part_of_speech[key]))
+#     token_by_part_of_speech[key] = [str(x).lower() for x in token_by_part_of_speech[key] if
+#                                     "." not in str(x) and "#" not in str(x) and ";" not in str(x) and "}" not in str(
+#                                         x) and "{" not in str(x) and "@" not in str(x)]
+# removed duplicates from lists and filtered out some trash from the lists (also lowercased all tokens)
+
+# print(token_by_part_of_speech)
+# print(token_by_part_of_speech.keys())
